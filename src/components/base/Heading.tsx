4732e5154c8ba0531d620ea0370983cf6a5f2aa1
zipang <christophe.desguez@gmail.com>
2025-03-05 23:48:33 +0100

    Define Heading custom sizes

--- src/components/base/Heading.tsx
+++ src/components/base/Heading.tsx
@@ -1,27 +1,15 @@
 // src/components/base/Heading.tsx
 import { Component } from "solid-js";
 import { styled, type HTMLStyledProps } from "@styled-system/jsx";
-import { defineTokens } from "@pandacss/dev";
-
-export const fontSizes = defineTokens.fontSizes({
-	xs: { value: "0.7rem" },
-	sm: { value: "0.875rem" },
-	md: { value: "1rem" },
-	lg: { value: "1.25rem" },
-	xl: { value: "1.7rem" },
-	xxl: { value: "2.25rem" },
-	"4xl": { value: "3rem" }
-});
+import { textStyles } from "@theme/typography";
 
 export interface HeadingProps extends HTMLStyledProps<"h1"> {
 	as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-	size?: keyof typeof fontSizes;
+	size?: keyof typeof textStyles;
 }
 
-export const Heading = styled("h1", {
-	base: {
-		fontFamily: "Garamond, Baskerville, Georgia, 'Noto Serif', TimesNewRomanPSMT, Times, serif",
-		fontWeight: 700,
-		lineHeight: 1.5
-	}
-}) as Component<HeadingProps>;
+const HeadingComponent = styled("h1") as Component<HeadingProps>;
+
+export const Heading: Component<HeadingProps> = ({ as = "h2", size = "xl", ...more }) => {
+	return <HeadingComponent as={as} {...textStyles[size].value} {...more} />;
+};
