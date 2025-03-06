e93bc7d05f2c981e47c2c763797c08cedf470a6a
zipang <christophe.desguez@gmail.com>
2025-03-06 20:17:30 +0100

    Add scss support and alternate mean of doing semantic CSS (Panda CSS is not good for all situations)

--- src/components/ui/BackgroundImage.tsx
+++ src/components/ui/BackgroundImage.tsx
@@ -1,35 +1,67 @@
-import { Component } from "solid-js";
+import { Component, JSX, ParentComponent } from "solid-js";
 import { Box } from "@components/base";
-import "./background-image-styles.css";
+import "./background-image-styles.scss";
 
 export interface BackgroundImageProps {
 	src: string;
 	mode: "repeat" | "cover";
-	position?: "fixed" | "scroll";
+	fixed?: boolean;
+}
+
+export type WithBackgroundImageProps<P> = P & {
+	backgroundImage?: BackgroundImageProps;
+};
+
+const mergeAllProps: (p: WithBackgroundImageProps<Record<string, any>>) => Record<string, any> = ({
+	backgroundImage,
+	...initialProps
+}) => {
+	const classList = {
+		"w-background-image": true,
+		fixed: Boolean(backgroundImage.fixed),
+		repeat: backgroundImage.mode === "repeat",
+		...initialProps.classList
+	};
+	initialProps.style = {
+		...initialProps.style,
+		"background-image": `url(${backgroundImage.src})`
+	};
+
+	return {
+		...initialProps,
+		classList
+	};
+};
+
+/**
+ * HOC to decorate an existing component with the additional `backgroundImage` prop behaviour
+ * @param WrappedComponent
+ * @returns
+ */
+export function withBackgroundImage<P extends Record<string, any>>(
+	WrappedComponent: Component<P>
+): Component<WithBackgroundImageProps<P>> {
+	return (props: WithBackgroundImageProps<P>) => {
+		const withBackgroundImageProps = mergeAllProps(props);
+
+		return <WrappedComponent {...(withBackgroundImageProps as P)} />;
+	};
 }
 
 /**
- * Render a background image inside a container
+ * Adds an image child inside a container that will render as a background image
  * The parent must have a `position: relative` and `overflow: hidden` style preset
- * There is two modes : repeat and cover
  */
 export const BackgroundImage: Component<BackgroundImageProps> = ({
 	src,
 	mode = "cover",
-	position
+	fixed = false
 }) => {
 	if (mode === "cover") {
 		return <img src={src} class="background-image-cover" alt="background image" />;
 	}
 
 	// Use a div with a repeating background
-	return (
-		<Box
-			class="background-image-repeat"
-			backgroundAttachment={position}
-			style={{
-				"background-image": `url(${src})`
-			}}
-		/>
-	);
+	const backgroundImageProps = mergeAllProps({ backgroundImage: { src, mode, fixed } });
+	return <Box {...backgroundImageProps} />;
 };
