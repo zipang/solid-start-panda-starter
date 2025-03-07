b091a94d334d219dc4e70958c621b218199443f9
zipang <christophe.desguez@gmail.com>
2025-03-07 21:13:10 +0100

    Rely on predefined class names because dynamic values like `gridTemplateColumns={new Array(columns).fill("1fr").join(" ")}` don't work with Panda CSS

--- src/components/base/Grid.tsx
+++ src/components/base/Grid.tsx
@@ -1,11 +1,30 @@
 import { Component } from "solid-js";
 import { Box, type BoxProps } from "./Box";
+import "./grid-styles.scss";
+
+// These classes have responsive props in the scss file
+const cols = ["", "", "two-columns", "three-columns", "four-columns"];
 
 export interface GridProps extends BoxProps {
-	columns: number;
+	columns: 1 | 2 | 3 | 4;
 	gap?: number | string;
+	// Tell how an image fit into a grid item
+	imageFit?: "cover" | "fit";
 }
 
-export const Grid: Component<GridProps> = ({ columns = 3, gap = "2rem", ...more }) => (
-	<Box display="grid" gridTemplateColumns={new Array(columns).fill("1fr").join(" ")} {...more} />
-);
+export const Grid: Component<GridProps> = ({
+	columns = 3,
+	gap = "2rem",
+	imageFit = "fit",
+	children,
+	...more
+}) => {
+	const gridItems = Array.isArray(children) ? children : [children];
+	return (
+		<Box class={`grid ${cols[columns]}`} gap={gap} {...more}>
+			{gridItems.map((child) => (
+				<div class={`grid-item image-${imageFit}`}>{child}</div>
+			))}
+		</Box>
+	);
+};
