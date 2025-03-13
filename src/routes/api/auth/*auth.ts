572f2e63d6cb2e8bf74e132d49da0b1dfa0ce2fb
zipang <christophe.desguez@gmail.com>
2025-03-13 23:30:43 +0100

    fix the auth route

--- src/routes/api/auth/*auth.ts
+++ src/routes/api/auth/*auth.ts
@@ -2,8 +2,8 @@ import { auth } from "@lib/auth";
 import { toSolidStartHandler } from "better-auth/solid-start";
 
 /**
- * Handle API routes:
- * GET /api/auth
- * POST /api/auth
+ * Handle any API routes:
+ * GET /api/auth/*
+ * POST /api/auth/*
  */
 export const { GET, POST } = toSolidStartHandler(auth);
