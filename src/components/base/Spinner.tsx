f111ae0eebcf9da52c7147d27ebfa5eb4bd051bd
zipang <christophe.desguez@gmail.com>
2025-03-16 21:37:48 +0100

    New Spinner composant (based on Chakra UI implementation)

---
+++ src/components/base/Spinner.tsx
@@ -0,0 +1,17 @@
+import { type Component } from "solid-js";
+import { Box } from "@components/base";
+import "./spinner-styles.css";
+
+export interface SpinnerProps {
+	size?: string | number;
+	thickness?: string | number;
+	color?: string;
+}
+
+export const Spinner: Component<SpinnerProps> = ({
+	size = "1rem",
+	thickness = "2px",
+	color = "currentcolor"
+}) => (
+	<Box class="spinner" height={size} width={size} borderWidth={thickness} borderColor={color} />
+);
