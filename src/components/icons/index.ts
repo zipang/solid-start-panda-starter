02514ea53ca852a5766710bf3ae6798da13311ef
zipang <christophe.desguez@gmail.com>
2025-03-17 21:33:58 +0100

    New SVG icons components

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,7 @@
+export * from "./AppleLogo";
 export * from "./GithubLogo";
 export * from "./GoogleLogo";
+export * from "./MicrosoftLogo";
 
 export interface IconProps {
 	size?: string | number;
