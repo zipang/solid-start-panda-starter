c1a670ce7968daf9fc6aec08aa8db808b727cf04
zipang <christophe.desguez@gmail.com>
2025-03-17 21:41:12 +0100

    Switch between Avatar and LOGIN action

---
+++ src/components/auth/UserActions.tsx
@@ -0,0 +1,19 @@
+import { Show, type Component } from "solid-js";
+import { Avatar, Link } from "@components/base";
+import { useSession } from "@lib/SessionContext";
+
+export const LoginButton: Component = () => (
+	<Link href="/login" preload={true} fontWeight="bolder" textDecoration="none">
+		LOGIN
+	</Link>
+);
+
+export const UserActions: Component = () => {
+	const session = useSession();
+	return (
+		<Show when={session.user} fallback={<LoginButton />}>
+			{/* @ts-expect-error session.user is possibly null */}
+			<Avatar image={session.user.image} title={session.user.name} />
+		</Show>
+	);
+};
