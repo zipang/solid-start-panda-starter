a1e3cf266c90d9bb87ca0346bdc59cf476afedd3
zipang <christophe.desguez@gmail.com>
2025-03-20 22:11:29 +0100

    Missing secrets..

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,21 @@
 import { betterAuth } from "better-auth";
 
+const SECRETS = [
+	"GITHUB_CLIENT_ID",
+	"GITHUB_CLIENT_SECRET",
+	"GOOGLE_CLIENT_ID",
+	"GOOGLE_CLIENT_SECRET"
+];
+const missingSecrets = SECRETS.filter((secret) => !process.env[secret]);
+
+if (missingSecrets.length > 0) {
+	throw new Error(
+		`Missing environnement variables. Add the required variables to your env: '${missingSecrets.join("', '")}'`
+	);
+}
+
 const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } =
 	process.env;
-
-if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET || !GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
-	throw new Error(`Missing environnement variables. Add the required variables to your env.`);
-}
 
 export const auth = betterAuth({
 	basePath: "/api/auth",
@@ -13,7 +23,7 @@ export const auth = betterAuth({
 		// @see https://www.better-auth.com/docs/guides/optimizing-for-performance#cookie-cache
 		cookieCache: {
 			enabled: true,
-			maxAge: 5 * 60, // Cache duration in seconds
+			maxAge: 5 * 60 // Cache duration in seconds
 		}
 	},
 	socialProviders: {
