3213c3dd1ba4aae0afc4ddbedb40c114e96e5b7c
zipang <christophe.desguez@gmail.com>
2025-03-20 22:10:46 +0100

    Expose how the auth API routes are build on top of the better-auth handler

--- src/routes/api/auth/*auth.ts
+++ src/routes/api/auth/*auth.ts
@@ -1,9 +1,18 @@
+import type { APIEvent } from "@solidjs/start/server";
 import { auth } from "@lib/auth";
-import { toSolidStartHandler } from "better-auth/solid-start";
+
+/**
+ * Solid Start API routes receive an API Event
+ * @see https://docs.solidjs.com/solid-start/building-your-application/api-routes#writing-an-api-route
+ * @param event APIEvent
+ * @returns Promise<Response>
+ */
+const authHandler = async (event: APIEvent) => auth.handler(event.request);
 
 /**
  * Handle any API routes:
  * GET /api/auth/*
  * POST /api/auth/*
  */
-export const { GET, POST } = toSolidStartHandler(auth);
+export const GET = authHandler;
+export const POST = authHandler;
