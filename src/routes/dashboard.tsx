a554609bf00cbcd1eb2eb8c876fcd5d963d7b270
zipang <christophe.desguez@gmail.com>
2025-03-21 23:49:00 +0100

    More example actions to change the current session (needs refresh)

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,11 +1,13 @@
 import { Title } from "@solidjs/meta";
 import { Box, Button, Container, Heading, Text, VStack } from "@components/base";
 import { AppHeader } from "@components/app";
-import { protectedRoute } from "@components/auth/Session";
+import { protected$, useSession } from "@components/auth";
 
 const TITLE = "SolidJS + Panda UI Starter - DASHBOARD";
 
-const LoginPage = protectedRoute(() => {
+const DashboardPage = protected$(() => {
+	const session = useSession();
+
 	return (
 		<main>
 			<AppHeader />
@@ -15,19 +17,38 @@ const LoginPage = protectedRoute(() => {
 			<Box as="section" height="dvh" border="1px solid #ccc">
 				<Container>
 					<Heading color="black">{TITLE}</Heading>
-					<Text>
-						This is a protected route. Try login out to be automatically redirect to the
-						login page.
-					</Text>
 					<VStack>
-						<Button variant="outline" size="xl">
+						<Text>
+							This is a protected route. Try login out to be automatically redirect to
+							the login page.
+						</Text>
+						<Button variant="outline" size="xl" onClick={() => session.signOut()}>
 							Logout
+						</Button>
+						<Text>
+							Try clicking the button below to set the logged user to John DOE.
+						</Text>
+						<Button
+							variant="outline"
+							size="xl"
+							onClick={() =>
+								session.update({
+									user: {
+										id: "1",
+										email: "john.doe@johns.com",
+										name: "John DOE",
+										image: "https://avatars.githubusercontent.com/u/149215215?v=4"
+									}
+								})
+							}
+						>
+							Log John Doe
 						</Button>
 					</VStack>
 				</Container>
 			</Box>
 		</main>
 	);
-});
+}, "/dashboard");
 
-export default LoginPage;
+export default DashboardPage;
