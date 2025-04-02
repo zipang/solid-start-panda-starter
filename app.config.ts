806c5237f0231c976265f027f49114c249000c55
zipang <christophe.desguez@gmail.com>
2025-04-02 22:27:23 +0200

    Use vite-tsconfig-paths plugin to sunc Typescript aliases with Vite

--- app.config.ts
+++ app.config.ts
@@ -1,27 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
-import { resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
-console.log(`Project root`, __dirname);
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	ssr: true,
-	server: {
-		baseURL: process.env.BASE_PATH,
-		preset: "static"
-	},
+	server: { baseURL: process.env.BASE_PATH, preset: "static" },
 
 	vite: {
-		resolve: {
-			alias: {
-				"@": resolve(__dirname, "./src"),
-				"@components": resolve(__dirname, "./src/components"),
-				"@lib": resolve(__dirname, "./src/lib"),
-				"@routes": resolve(__dirname, "./src/routes"),
-				"@theme": resolve(__dirname, "./src/theme"),
-				"@styled-system": resolve(__dirname, "./styled-system")
-			}
-		}
+		plugins: [tsconfigPaths()]
 	}
 });
