fe9f0e2e0a25cc766007dea898c4a35b8cd7caf3
zipang <christophe.desguez@gmail.com>
2025-04-11 22:21:55 +0200

    Extract createContext of SessionProvider in its own file to avoid HMR problems

--- src/components/auth/SessionProvider.tsx
+++ src/components/auth/SessionProvider.tsx
@@ -1,71 +1,13 @@
 import { authClient } from "@lib/auth-client";
-import { createContext, createSignal, useContext, type ParentComponent } from "solid-js";
+import { useContext, type ParentComponent } from "solid-js";
 import { createStore } from "solid-js/store";
 import { isServer } from "solid-js/web";
-
-export type SessionState = "empty" | "pending" | "alive";
-export interface SessionUser {
-	id: string;
-	name: string;
-	email: string;
-	image?: string;
-}
-export interface EmptySession {
-	state: "empty";
-	user: null;
-	token: null;
-	created: null;
-	expires: null;
-}
-export interface PendingSession {
-	state: "pending";
-	user: null;
-	token: null;
-	created: null;
-	expires: null;
-}
-export interface LiveSession {
-	state: "alive";
-	user: SessionUser;
-	token: string;
-	created: string;
-	expires: string;
-}
-
-export interface SessionContext {
-	/**
-	 * Retrieves the _current_ session state while optionally forcing a refresh
-	 */
-	session: (prop: { forceRefresh?: boolean }) => LiveSession | PendingSession | null;
-	/**
-	 * Currently TRUE when nothing is known about the session or when there is NO logged user
-	 */
-	isEmpty: () => boolean;
-	/**
-	 * Shortly TRUE after a new signIn() before we retrieve the session data
-	 */
-	isPending: () => boolean;
-	/**
-	 * TRUE when the session is active with a logged user
-	 */
-	isLive: () => boolean;
-	/**
-	 * Retrieves the _current_ user (not null only when session is alive)
-	 */
-	user: () => SessionUser | null;
-	/**
-	 * Force refreshing the current session state
-	 */
-	refresh: () => Promise<void>;
-	/**
-	 * Close the current session and force the user to log in again
-	 */
-	signOut: () => Promise<void>;
-	/**
-	 * Update the session state
-	 */
-	update: (session: Partial<LiveSession | PendingSession> | null) => void;
-}
+import {
+	SESSION_CONTEXT,
+	type EmptySession,
+	type SessionState,
+	type SessionUser
+} from "./SessionContext";
 
 const EMPTY_SESSION = {
 	state: "empty",
@@ -75,24 +17,7 @@ const EMPTY_SESSION = {
 	expires: null
 } satisfies EmptySession;
 
-const EMPTY_SESSION_CONTEXT = {
-	session: () => null,
-	user: () => null,
-	refresh: async () => {},
-	signOut: async () => {},
-	isEmpty: () => true,
-	isPending: () => false,
-	isLive: () => false,
-	update: () => null
-} satisfies SessionContext;
-
-const SESSION_CONTEXT = createContext<SessionContext>(EMPTY_SESSION_CONTEXT);
-
 export const useSession = () => {
-	if (isServer) {
-		return EMPTY_SESSION_CONTEXT;
-	}
-
 	const sessionContext = useContext(SESSION_CONTEXT);
 
 	if (!sessionContext) {
@@ -103,9 +28,7 @@ export const useSession = () => {
 };
 
 export const SessionProvider: ParentComponent = (props) => {
-	const [observableSession, setObservableSession] = createStore<
-		LiveSession | PendingSession | EmptySession
-	>(EMPTY_SESSION);
+	const [observableSession, setObservableSession] = createStore<SessionState>(EMPTY_SESSION);
 
 	const isEmpty = () => observableSession.state === "empty";
 	const isPending = () => observableSession.state === "pending";
@@ -114,17 +37,22 @@ export const SessionProvider: ParentComponent = (props) => {
 
 	const signOut = async () => {
 		authClient.signOut();
-		setObservableSession({ ...EMPTY_SESSION });
+		setObservableSession(EMPTY_SESSION);
 	};
 
-	const session = ({ forceRefresh = false }) => {
-		if (forceRefresh) {
-			refresh();
-		}
+	const data = () => {
 		return observableSession.state === "empty" ? null : observableSession;
 	};
 
 	const refresh = async () => {
+		if (isServer) {
+			return;
+		}
+
+		if (observableSession.state !== "alive") {
+			setObservableSession({ state: "pending" });
+		}
+
 		authClient
 			.getSession()
 			.then(({ data, error }) => {
@@ -144,7 +72,8 @@ export const SessionProvider: ParentComponent = (props) => {
 				if (error) {
 					throw error;
 				}
-				setObservableSession(EMPTY_SESSION);
+				setObservableSession({ state: "empty" });
+				console.log("Set session to empty", observableSession.state);
 			})
 			.catch((error) => {
 				console.error(`Session retrieval failed with error : ${error.message}`, error);
@@ -152,7 +81,7 @@ export const SessionProvider: ParentComponent = (props) => {
 			});
 	};
 
-	const update = (session: Partial<LiveSession | PendingSession> | null) => {
+	const update = (session: Partial<SessionState> | null) => {
 		if (session === null) {
 			setObservableSession(EMPTY_SESSION);
 			return;
@@ -166,7 +95,7 @@ export const SessionProvider: ParentComponent = (props) => {
 	return (
 		<SESSION_CONTEXT.Provider
 			value={{
-				session,
+				data,
 				isEmpty,
 				isPending,
 				isLive,
